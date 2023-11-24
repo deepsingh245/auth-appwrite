a9ca3ca70faf1967731a54b2992a6bcccb9653ef
Deep Singh <deepsingh25244@gmail.com>
2023-11-24 15:47:21 +0530

    launched

---
+++ src/conf/config.ts
@@ -0,0 +1,8 @@
+const config = {
+appwriteUrl: String(process.env.
+    NEXT_PUBLIC_APPWRITE_URL),
+    appwriteProjectId: String(process.env.
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+
+}
+export default config
